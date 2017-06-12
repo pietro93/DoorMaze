f5d75587136f06c1c765ee5d6d2a1d61401a09f6
Pietro Romeo <xpietro93@hotmail.com>
2017-06-12 22:51:16 +0200

    Updated game to accomodate actual sounds for the experiment.

--- js/game.js
+++ js/game.js
@@ -55,7 +55,7 @@ function startGame() {
 /* To assure sounds are played in random order */
 function randomiseSounds() {
     // populate array
-    for (i = 1; i <= 24; i++) { sounds.push(i) }
+    for (i = 1; i <= 18; i++) { sounds.push(i) }
     // shuffle array
     sounds.sort(function () {
         return .5 - Math.random();
@@ -75,19 +75,18 @@ function nextSound() {
 function chooseSound(n) {
     var sound = "";
   
-    //  E.g. if n=1 then sound = 0000. Sound format "ABCD" -> A: Intention - B: Gender - C: Length - D: Feature
-    if (n <= 12) { sound += "0"} else { sound += "1" };
-    if (n <= 6 || (13 <= n && n <= 18)) { sound += "0" } else { sound += "1" };
-    if (n <= 3 || (7 <= n && n <= 9) || (13 <= n && n <= 15) || (19 <= n && n <= 21)) { sound += "0" } else { sound += "1" };
-    switch (n % 3) {
-        case 1:
-            sound += "0"; break;
-        case 2:
-            sound += "1"; break;
+    //  E.g. if n=1 then sound = 0000. Sound format "ABCD" -> A: Base - B: Intention - C: Shape - D: Gender
+    if (n <= 9) { sound += "B"} else { sound += "H" };
+    if (n <= 4 || (9 <= n && n <= 12)) { sound += "A" } else { sound += "D" };
+    if (n <= 2 || (5 <= n && n <= 6) || (9 <= n && n <= 10) || (13 <= n && n <= 14)) { sound += "B" } else { sound += "T" };
+    switch (n % 2) {
         case 0:
-            sound += "2"; break;
+            sound += "F"; break;
+        case 1:
+            sound += "M"; break;
+      
     }
-
+    if (n == 17) {sound="BN"} else if (n==18) {sound="HN"}
     return sound
     }
 
@@ -109,7 +108,7 @@ function setScreenLayout(enterDoor) {
         door = "door" + Math.floor((Math.random() * 9))
 
         /* First game screen, introduce scenario */
-        if (sounds.length == 23) {
+        if (sounds.length == 17) {
             action = "You are in a maze dungeon with your robot companion. It will guide you outside if you listen to it - every time you approach a door, it will tell you whether you should open it or search for a different one."
         }
 
@@ -126,16 +125,12 @@ function setScreenLayout(enterDoor) {
         $("#container > .text").fadeIn(5000)
         
         // Set sound player according to current sound
-        if (sound[0] == 0) { setPlayer("yes") } else { setPlayer("no") } // these are just test sounds
-        /*
         setPlayer(sound);
-        */
 
         // Play sound when players hovers mouse over door
         $(document).ready(function () {
             $("#door").hover(function () {
                 audio.play();
-                console.log("playing")
                 $("#container > .question").fadeIn(4000)
             },
             function () {
@@ -149,10 +144,10 @@ function setScreenLayout(enterDoor) {
 /* store participant action and update score accordingly */
 function submit(enterDoor) {
     readSound(sound)
-    localStorage.setItem(sound, + !enterDoor)
+    localStorage.setItem(sound, + !enterDoor) // stored as 0 = did not enter; 1 = entered
     
     // Increase score if player guessed robot intention correctly
-    if (sound[0] != enterDoor) { score += 1 }
+    if ((sound[1] == "A" && enterDoor) || (sound[1] == "D" && !enterDoor))  { score += 1 }
     console.log("Robot feedback: " + intention + " Player entered door: " + enterDoor)
     console.log("Current score: "+score)
     
@@ -166,14 +161,14 @@ function complete() {
     /*generate CSV file with participant data*/
     csvContent = "data:text/csv;charset=utf-8,";
     csvContent += "P.NUM, GENDER, AGE"
-    for (n = 1; n <= 24; n++) {
-        csvContent += ",S" + chooseSound(n)
+    for (n = 1; n <= 18; n++) {
+        csvContent += "," + chooseSound(n)
     }
     csvContent += ",SCORE" + "\n"
 
     csvContent += localStorage.getItem("pnum") + "," + localStorage.getItem("gender") + "," + localStorage.getItem("age")
 
-    for (n = 1; n <= 24; n++) {
+    for (n = 1; n <= 18; n++) {
         csvContent += "," + localStorage.getItem(chooseSound(n))
     }
 
@@ -199,7 +194,7 @@ function complete() {
         action = "You have found your way out of the dungeon."
         $("#container").append('<img id="door" src="images/outside.png" style="width: 100%; height: 100%" />')
         $("#container").append('<p class="score shadow1" style:"position: absolute; top: 25%">' + action + '</p>')
-        $("#container").append('<p class="score shadow2"> Your score is <font style="font-weight: bold" color="gold">' + score + '</font> out of 24.</p>')
+        $("#container").append('<p class="score shadow2"> Your score is <font style="font-weight: bold" color="gold">' + score + '</font> out of 16.</p>')
        
         $("#container").fadeIn(5000)
     }, 2000);
--- js/sound.js
+++ js/sound.js
@@ -1,39 +1,41 @@
-var intention, gender, length, feature;
+var intention, gender, base, shape;
 
 function readSound(sound){
     switch (sound[0]) {
-        case "0": 
-           intention = "approval"
+        case "B": 
+           base = "beep"
             break;
-        case "1":
-            intention = "disapproval";
-            break;
-    }
-    switch (sound[1]){
-        case "0":
-            gender = "male"
-            break;
-        case "1":
-            gender = "female";
+        case "H":
+            base = "human";
             break;
     }
     switch (sound[2]){
-        case "0":
-            sound.length = "short"
+        case "B":
+            shape = "bottom"
             break;
-        case "1":
-            sound.length = "long";
+        case "T":
+            shape = "top";
             break;
     }
     switch (sound[3]){
-        case "0":
-            sound.feature = "pitch"
+        case "F":
+            gender = "female"
             break;
-        case "1":
-            sound.feature = "intensity"
+        case "M":
+            gender = "male"
             break;
-        case "2":
-            sound.feature = "pitch x intensity";
+    }
+    switch (sound[1]) {
+        case "A":
+            intention = "approval"
+            break;
+        case "D":
+            intention = "disapproval"
+            break;
+        case "N":
+            intention = "control"
+            gender = "neutral"
+            shape = "none"
             break;
     }
 }
@@ -44,7 +46,7 @@ function setPlayer(sound) {
             $("#container").append('<center><div id="player" class="player" style="position:absolute; display:none; visibility: hidden;"> </center>')
             $("#player").append('<center><audio id="audio" src="' + fileloc + '" controls/></center>')
             console.log("Playing: " + fileloc)
-            console.log("Intention: " + intention + " Gender: " + gender + " Length: " + length + " Feature: " + feature)
+            console.log("Intention: " + intention + " Gender: " + gender + " Base: " + base + " Shape: " + shape)
 }
 
 function stop() {
